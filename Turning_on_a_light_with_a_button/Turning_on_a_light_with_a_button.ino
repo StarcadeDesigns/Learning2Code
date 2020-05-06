81e247d6a7dce49b11d959109714a435b6a76b53
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-06 08:47:15 -0500

    Update Turning_on_a_light_with_a_button.ino

--- Turning_on_a_light_with_a_button/Turning_on_a_light_with_a_button.ino
+++ Turning_on_a_light_with_a_button/Turning_on_a_light_with_a_button.ino
@@ -12,11 +12,11 @@ pinMode(BUTTON_PIN, INPUT);  // making the button an input on the board???
 
 void loop() {
   // put your main code here, to run repeatedly:
-buttonState = digitalRead(BUTTON_PIN);
+buttonState = digitalRead(BUTTON_PIN); // this tells the CPU to check if button is pressed
 
 if (buttonState == HIGH) {
-  digitalWrite(GREEN_LED_PIN, HIGH);
+  digitalWrite(GREEN_LED_PIN, HIGH); // if button is pressed the output should be high meaning the light is on
 } else {
-  digitalWrite(GREEN_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW); // if the button is not pressed the ouput should be low meaning the light is off
 }
 }
