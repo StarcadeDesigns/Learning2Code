7c9302f7bf8023d7c27a8b76c2f0f0e62a4fce73
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-05 22:18:58 -0500

    First C++ file

---
+++ Hello_World/Hello_World.ino
@@ -0,0 +1,24 @@
+#define START_TEXT "SYN Snugglez" // this is the start up text to appear 
+char playerUsername = "Spongebob"; // for the player to use to put in a username
+
+/*
+ *  Constant name = #define UPPER_CASE
+ *  Variable name = (int,char,bool,unsigned long) chadIsDope 
+ *  Function = myNameIsMatthew ()
+ * ; is needed after all variables
+ * You don't have to give a varibale a value but its reccomnended 
+ * "" is needed for strings of text
+ * '' for single letter 
+ * numbers dont need ticks
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
