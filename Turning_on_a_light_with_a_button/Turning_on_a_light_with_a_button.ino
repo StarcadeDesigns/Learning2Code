e4436913567ce15c796f336ee6be201a515d8854
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-06 08:40:40 -0500

    Update Turning_on_a_light_with_a_button.ino

--- Turning_on_a_light_with_a_button/Turning_on_a_light_with_a_button.ino
+++ Turning_on_a_light_with_a_button/Turning_on_a_light_with_a_button.ino
@@ -1,23 +1,20 @@
-/*
- * KNOWN ISSUES:
- * pinMode(GREEN_LED_PIN), OUTPUT); there is an error of too few arguements to funtion
- */
-
 #define GREEN_LED_PIN A5 // LED will turn on if the button is pressed 
 #define BUTTON_PIN A0 // The button will turn the LED light on while it is pressed
 
 int buttonState = 0;
 
 void setup() {
-pinMode(GREEN_LED_PIN, OUTPUT); // telling the board this pin should be an ouput for power/signal
-pinMode(BUTTON_PIN, INPUT);  // telling the board that this pin should be an input for power/signal
+pinMode(GREEN_LED_PIN, OUTPUT); // making the light an output i think however there is an error of too few arguements to funtion
+pinMode(BUTTON_PIN, INPUT);  // making the button an input on the board???
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-buttonState = digitalRead(BUTTON_PIN); // WHAT DOES THIS DO, WHAT DO YOU THINK IT DOES :)
+buttonState = digitalRead(BUTTON_PIN);
 
-if (buttonState == HIGH) { // NO COMMENTS HERE. I KNOW WHAT IT DOES BUT NOT WHAT YOU WANT IT TO DO.
+if (buttonState == HIGH) {
   digitalWrite(GREEN_LED_PIN, HIGH);
 } else {
   digitalWrite(GREEN_LED_PIN, LOW);
