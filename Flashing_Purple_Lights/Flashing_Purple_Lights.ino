de109d77152f75ceaae6cb3627bfc06b32b21741
AziDesigns <chadwegrecki@gmail.com>
2020-05-06 16:32:53 -0500

    Fixed delay logic

--- Flashing_Purple_Lights/Flashing_Purple_Lights.ino
+++ Flashing_Purple_Lights/Flashing_Purple_Lights.ino
@@ -4,7 +4,7 @@
  */
 
 #define PURPLE_LED_PIN 13 // The LED is Purple it will go on and off with a delay of .5 seconds
-#define LED_DELAY 11
+#define LED_DELAY 500 // .5 second delay
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,9 +14,9 @@ pinMode (PURPLE_LED_PIN, OUTPUT); // set the LED as an OUTPUT
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(PURPLE_LED_PIN,HIGH); // on
-delay(LED_DELAY, 500); // .5 second delay
+delay(LED_DELAY); 
 digitalWrite(PURPLE_LED_PIN,LOW); // off
-delay(LED_DELAY, 500); // .5 second delay
+delay(LED_DELAY); 
 /* assuming I did this correctly this will repated for all eternity
  *  I mostly ran this code so that I could check to see an error in a differnent code but this one runs fine whcih makes the other code that much more confusing
  */
