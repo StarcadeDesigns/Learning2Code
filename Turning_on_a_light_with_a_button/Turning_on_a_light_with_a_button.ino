a78fec478f180ee66c7239bc13f7bc12fec28328
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-06 07:37:01 -0500

    Create Turning_on_a_light_with_a_button.ino
    Too many arguements  in line

---
+++ Turning_on_a_light_with_a_button/Turning_on_a_light_with_a_button.ino
@@ -0,0 +1,23 @@
+#define GREEN_LED_PIN A5 // LED will turn on if the button is pressed 
+#define BUTTON_PIN A0 // The button will turn the LED light on while it is pressed
+
+int buttonState = 0;
+
+void setup() {
+pinMode(GREEN_LED_PIN), OUTPUT); // making the light an output i think
+
+pinMode(BUTTON_PIN, INPUT);  // making the button an input on the board???
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+buttonState = digitalRead(BUTTON_PIN);
+
+if (buttonState == HIGH) {
+  digitalWrite(GREEN_LED_PIN, HIGH);
+} else {
+  digitalWrite(GREEN_LED_PIN, LOW);
+}
+}
