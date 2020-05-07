ea44119b1e61ccdd717492b4ea8f52492922edc9
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-06 19:21:06 -0500

    Update I_don_t_believe_in_Harvey_Dent.ino

--- I_don_t_believe_in_Harvey_Dent/I_don_t_believe_in_Harvey_Dent.ino
+++ I_don_t_believe_in_Harvey_Dent/I_don_t_believe_in_Harvey_Dent.ino
@@ -2,6 +2,8 @@
 char playerUsername = "Two Face"; // Default Usernam feel free to change it
 int countDown = 60; // you have 60 seconds to put in your username before you die. 
 long randNumber; 
+#define BUTTON_KILL A5 // this button gives up the game and makes you forfit 
+int buttonState = 0;
 
 
 
@@ -10,6 +12,7 @@ void setup() {
 Serial.begin(9600);
 Serial.println(WELCOME_SCREEN);
 Serial.println(playerUsername);
+pinMode(BUTTON_KILL, INPUT); // kills player 
 
 }
 
@@ -20,8 +23,14 @@ Serial.println(countDown); // I thnk this will count down to 0
 delay(1000); // goes down by 1 second
 randNumber = random(0-1000); // gets a random number and you are looking to get a prime number
 delay(1000); //  get a new number every 1 second
-}
+buttonState = digitalRead(BUTTON_KILL); // this tells the CPU to check if button is pressed
 
+if (buttonState == HIGH) {
+  digitalWrite(BUTTON_KILL, HIGH); // if button is pressed the output should be high meaning the player has forfited 
+  } else {
+  digitalWrite(BUTTON_KILL, LOW); // if the button is not pressed the ouput should be low meaning the player hasn't forfited 
+}
+}
 
 
 /* the entire game is putting the username in in 60 seconds and then getting a prime number from the RNG
