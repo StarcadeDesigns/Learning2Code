030316dbe81a9f04fc1134ff68e8aa02190bcfa7
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-07 07:15:52 -0500

    Update I_don_t_believe_in_Harvey_Dent.ino

--- I_don_t_believe_in_Harvey_Dent/I_don_t_believe_in_Harvey_Dent.ino
+++ I_don_t_believe_in_Harvey_Dent/I_don_t_believe_in_Harvey_Dent.ino
@@ -1,43 +1,47 @@
-#define WELCOME_SCREEN "Don't Believe in Harvey Dent" // Game Name
-char playerUsername = "Two Face"; // Default Usernam feel free to change it
-int countDown = 60; // you have 60 seconds to put in your username before you die. 
-long randNumber; 
-#define BUTTON_KILL A5 // this button gives up the game and makes you forfit 
-int buttonState = 0;
-
 /*
- * need:
- * bool
- * delayTime
+ * KNOWN ISSUES:  I think I am still missing something in my loop but I am not able to rememeber or identify it.
  * 
  */
 
 
+#define WELCOME_SCREEN "Don't Believe in Harvey Dent" // Game Name
+#define BUTTON_KILL A5 // this button gives up the game and makes you forfit 
+
+char playerUsername = "Two Face"; // Default Usernam feel free to change it
+int countDown = 60; // you have 60 seconds to put in your username before you die. 
+unsigned long previousMillis = 0;
+unsigned long startMillis;
+unsigned long currentMillis;
+const long interval =1000;
+long randNumber; 
+int buttonState = 0;
+bool gameOver = false;
+
+
+
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-Serial.println(WELCOME_SCREEN);
-Serial.println(playerUsername);
-pinMode(BUTTON_KILL, INPUT); // tells arduino to end loop
+  Serial.begin(9600);
+  Serial.println(WELCOME_SCREEN);
+  Serial.println(playerUsername);
+  pinMode(BUTTON_KILL, INPUT); // tells arduino to end loop
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(countDown); // I thnk this will count down to 
-0countDown--; // ideally this counts down 
-delay(1000); // goes down by 1 second
-randNumber = random(0-1000); // gets a random number and you are looking to get a prime number
-buttonState = digitalRead(BUTTON_KILL); // this tells the CPU to check if button is pressed
+  Serial.println(countDown); // I thnk this will count down to 
+  countDown--; // ideally this counts down 
+  if (currentMillis - previousMillis >= interval) {
+  previousMillis = currentMillis;
+  randNumber = random(0-1000); // gets a random number and you are looking to get a prime number
+  buttonState = digitalRead(BUTTON_KILL); // this tells the CPU to check if button is pressed
 
-if (buttonState == HIGH) {
-  digitalWrite(BUTTON_KILL, HIGH); // if button is pressed the output should be high meaning the player has forfited 
-  } else {
-  digitalWrite(BUTTON_KILL, LOW); // if the button is not pressed the ouput should be low meaning the player hasn't forfited 
+    if (buttonState == HIGH) 
+      gameOver = true;
+ 
 }
 }
-
-
 /* the entire game is putting the username in in 60 seconds and then getting a prime number from the RNG
  *  
  */
