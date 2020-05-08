b60f951db6f9584badf9e480acac6880c45a9248
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-08 10:23:59 -0500

    Update DnD_Hit_or_Miss.ino

--- DnD_Hit_or_Miss/DnD_Hit_or_Miss.ino
+++ DnD_Hit_or_Miss/DnD_Hit_or_Miss.ino
@@ -1,5 +1,5 @@
 #define PLAYER_NAME "Sir Maplebottom"
-#define ROLL_BUTTON A5 // Button will start the RNG 
+#define ROLL_BUTTON 3 // Button will start the RNG 
 
 long d20Roll;
 int buttonState = 0;
@@ -15,7 +15,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   buttonState = digitalRead(ROLL_BUTTON); // The RNG shouldn't happen if button isn't pressed
-  Serial.print(ROLL_BUTTON);
+  Serial.print(buttonState);
   if (buttonState == HIGH) {
     d20Roll = random(1,21);
     //Serial.print(d20Roll);
