96cd2d1c38434f158380728fbd424b4927f692a4
AziDesigns <chadwegrecki@gmail.com>
2020-05-08 10:15:38 -0500

    Update DnD_Hit_or_Miss.ino

--- DnD_Hit_or_Miss/DnD_Hit_or_Miss.ino
+++ DnD_Hit_or_Miss/DnD_Hit_or_Miss.ino
@@ -10,17 +10,14 @@ void setup() {
   
   Serial.begin(9600);
   Serial.println(PLAYER_NAME);
-  d20Roll = random(0-21);
-  Serial.print(d20Roll);
   pinMode(ROLL_BUTTON, INPUT); // tells ardunio that the button is an input
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- buttonState = digitalRead(ROLL_BUTTON); // The RNG shouldn't happen if button isn't pressed
-if (buttonState == LOW)
-    stopRng = false;  
-  
-  d20Roll = random(0,21);
-  Serial.print(d20Roll);
+  buttonState = digitalRead(ROLL_BUTTON); // The RNG shouldn't happen if button isn't pressed
+  if (buttonState == HIGH) {
+    d20Roll = random(1,21);
+    Serial.print(d20Roll);
+  }
 }
