561f319d5b229fab8180675ec68ca5cbecdf6e10
AziDesigns <chadwegrecki@gmail.com>
2020-05-08 10:47:37 -0500

    Update File Name & Serial

--- DnD_Hit_or_Miss/DnD_Hit_or_Miss.ino
+++ DnD_Hit_or_Miss/DnD_Hit_or_Miss.ino
@@ -3,6 +3,7 @@
 
 long d20Roll;
 int buttonState = 0;
+int incomingByte = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,6 +17,14 @@ void loop() {
   // put your main code here, to run repeatedly:
   buttonState = digitalRead(ROLL_BUTTON); // The RNG shouldn't happen if button isn't pressed
  // Serial.print(buttonState);
+ if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
   if (buttonState == HIGH) {
     d20Roll = random(1,21);
     Serial.print(d20Roll);
